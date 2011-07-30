e525c8a6a2fe9e4d3e2dd7574be2bf3a30bbd85f
guido <guido@westerwave.com>
2011-07-30 12:30:26 +0200

    added openccg-vollformen.py. this script extracts lexemes (or lemmas) from the OpenCCG lexicon and writes them to a file.

---
+++ lexicalization/openccg-vollformen.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Arne Neumann <arne-neumann@web.de>
+
+"""
+This script extracts all lexemes from the OpenCCG JPolibox lexicon.
+"""
+
+import re
+
+OPENCCG_LEXICON = open("/media/sync/current/nlg/openccg-jpolibox/dict.xml","r").readlines()
+
+def extract_lexemes(lexicon):
+    lexemes = []
+    lex_regex = re.compile('form=\"(.+?)\"')
+    for line in lexicon:
+        lexeme = lex_regex.findall(line) 
+        # findall returns a list (could possibly contain more than 1 item) 
+        # --> list.extend instead of list.append
+        lexemes.extend(lexeme)
+    return sorted(lexemes)
+
+def extract_lemmas(lexicon):
+    lemmas = []
+    lemma_regex = re.compile('stem=\"(.+?)\"')
+    for line in lexicon:
+        lemma = lex_regex.findall(line) 
+        # findall returns a list (could possibly contain more than 1 item) 
+        # --> list.extend instead of list.append
+        lemmas.extend(lemma)
+    return sorted(lemmas)
+
+    
+def write_to_file(word_list, file_name):
+    """write a list of words to a file, one word per line"""
+    word_file = open(file_name, "w")
+    for line in word_list:
+            word_file.write("{0}\n".format(line))
+    f.close()
