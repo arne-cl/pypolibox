6de1f9b2156ddda6c87a70789bf3ead5139d7735
guido <guido@westerwave.com>
2011-11-17 16:47:47 +0100

    added lexicalization to pypolibox.py. cosmetic changes to textplan.py

--- pypolibox.py
+++ pypolibox.py
@@ -17,11 +17,17 @@ from propositions import Propositions, AllPropositions
 from textplan import TextPlan, TextPlans, generate_textplan, linearize_textplan
 from messages import Message, Messages, AllMessages
 from rules import ConstituentSet, Rule, Rules
+from lexicalize_messageblocks import lexicalize_message_block
 import debug
 
 
 if __name__ == "__main__":
     query = Query(sys.argv[1:])
     books = Books(Results(query))
-    print books
-    print TextPlans(AllMessages(AllPropositions(AllFacts(books))))
+    textplans = TextPlans(AllMessages(AllPropositions(AllFacts(books))))
+    for textplan in textplans.document_plans[-3:]:
+    # realize only the three highest ranking books
+        msg_blocks = linearize_textplan(textplan)
+        for msg_block in msg_blocks:
+            lexicalized = lexicalize_message_block(msg_block)
+            print lexicalized
