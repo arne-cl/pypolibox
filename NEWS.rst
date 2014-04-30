d01cb7dffd027b71af044676d9d6996ceee38268
Arne Neumann <pypolibox.programming@arne.cl>
2014-04-30 16:29:05 +0200

    added setup.py, NEWS.rst and MANIFEST.in

---
+++ NEWS.rst
@@ -0,0 +1,17 @@
+.. This is your project NEWS file which will contain the release notes.
+.. Example: http://www.python.org/download/releases/2.6/NEWS.txt
+.. The content of this file, along with README.rst, will appear in your
+.. project's PyPI page.
+
+News
+====
+1.0.0
+-----
+
+*Release date: 30-Apr-2014*
+
+* pypolibox is now licensed under GPLv3
+* OpenCCG grammar fragment (CC-BY-NC-SA 4.0 licensed) now shipped with code
+* first release via PyPI
+* got rid of configuration file
+* fixed some errors in the documentation
---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+NEWS = open(os.path.join(here, 'NEWS.rst')).read()
+
+
+version = '1.0.0'
+
+install_requires = [
+    # List your project dependencies here.
+    # For more details, see:
+    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+    "lxml", "nltk", "pexpect"
+]
+
+
+setup(name='pypolibox',
+    version=version,
+    description="text generation for product recommendations using OpenCCG",
+    long_description=README + '\n\n' + NEWS,
+    # Get classifiers from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    # classifiers=[c.strip() for c in """
+    #     Development Status :: 4 - Beta
+    #     License :: OSI Approved :: MIT License
+    #     Operating System :: OS Independent
+    #     Programming Language :: Python :: 2.6
+    #     Programming Language :: Python :: 2.7
+    #     Programming Language :: Python :: 3
+    #     Topic :: Software Development :: Libraries :: Python Modules
+    #     """.split('\n') if c.strip()],
+    # ],
+    keywords='linguistics nlp nlg',
+    author='Arne Neumann',
+    author_email='pypolibox.programming@arne.cl',
+    url='https://github.com/arne-cl/pypolibox',
+    license='GPL Version 3',
+    packages=['pypolibox'],
+    package_dir = {'pypolibox': "src/pypolibox"},
+    package_data = {'pypolibox': ['data/*', 'grammar/*', 'avm.sty']},
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts':
+            ['pypolibox=pypolibox.pypolibox:main']
+    }
+)
